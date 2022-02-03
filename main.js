03fa508d41c8d3214ac37cfcb0eadc1fecf5e19e
Heukkell <falcfmpcv@gmail.com>
2022-02-03 12:24:43 +0900

    # 파일을 수정하여 업데이트 하는 로직 구성
    - POST 로 받은 내용으로, 파일 이름 변경 (fs.rename) , 파일 다시 쓰기(fs.write) 를 사용 하여 파일을 수정하여 업데이트 하도록 구성.

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ const app = http.createServer((request,response)=>{
                     var title = queryData.id;
                     var template = templatelib.templeteHTML(title,list,
                         `<h2>${title}</h2><p>${description}</p>`,
-                        `<a href="/create">create<a> <a href="/update">update<a>`);                
+                        `<a href="/create">create<a> <a href="/update?id=${title}">update<a>`);                
              
                     response.writeHead(200);
                     response.end(template);
@@ -46,14 +46,15 @@ const app = http.createServer((request,response)=>{
             });
         }
     }
-    else if(pathname === '/create'){
+    else if(pathname === '/create')
+    {
         fs.readdir('./data',(err,_filelist)=>{
             var title = 'welcome - create';
             var description = 'Hello, Node.js';
             var list=templatelib.templeteList(_filelist); // 파일명으로 주제를 담은 배열을 만들어 <li> 태그화
             var template=templatelib.templeteHTML(title,list,
             `
-                <form action="http://localhost:3000/create_process" method="post">
+                <form action="/create_process" method="post">
                     <p><input type="text" name="title" style="width:200px" placeholder="title"></p>
                     <p><textarea name="description" style="width:200px;height:300px" placeholder="description"></textarea></p>
                     <input type="submit"></input>
@@ -93,6 +94,56 @@ const app = http.createServer((request,response)=>{
                 }
             });
 
+        })
+    }
+    else if(pathname === '/update')
+    {   // 업데이트 요청시
+        fs.readdir('./data',(err,_filelist)=>{
+            var list=templatelib.templeteList(_filelist); // 파일명으로 주제를 담은 배열을 만들어 <li> 태그화
+            
+            fs.readFile(`data/${queryData.id}`,'utf8',(err,description)=>{
+                var title = queryData.id;
+                var template = templatelib.templeteHTML(title,list,
+                    `
+                    <form action="/update_process" method="post">
+                    <input type="hidden" name="id" value="${title}">
+                    <p><input type="text" name="title" style="width:200px" placeholder="title" value="${title}"></p>
+                    <p><textarea name="description" style="width:200px;height:300px" placeholder="description">${description}</textarea></p>
+                    <input type="submit"></input>
+                    </form>                    
+                    `,
+                    `<a href="/create">create<a> <a href="/update?id=${title}">update<a>`);                
+         
+                response.writeHead(200);
+                response.end(template);
+                })
+        });
+    }
+    else if(pathname === '/update_process')
+    {
+        var body = '';
+
+        request.on('data',(data)=>{
+            // 조각난 데이터 받기
+            body += data;
+        })
+        request.on('end',()=>{
+            var updatedData = qs.parse(body); // body 를 분석한 데이터를 받아.
+            // {id,title,description} 담겨있는데, id 에 해당하는 파일 삭제 하고, title 로 다시 만들고 안에는 description 넣고.
+            var oldFileName = updatedData.id;
+            var NewFileName = updatedData.title;
+            var description = updatedData.description;
+
+            fs.rename(`data/${oldFileName}`,`data/${NewFileName}`,(err)=>{
+                if(err){ /*에러처리 해야함 */};
+
+                fs.writeFile(`data/${NewFileName}`,description,`utf8`,(err)=>{
+                    if(err){/*에러처리 해야함 */};
+
+                    response.writeHead(302,{Location:`/?id=${NewFileName}`});
+                    response.end();
+                })
+            })
         })
     }
     else{
